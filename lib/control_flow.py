3c652d1829ba3355fb5fc68aebfa6679d37d2b25
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-10-03 23:52:16 +0300

    Assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,54 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    if username.lower() == "admin" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+
     pass
 
 def hows_the_weather(temperature):
-    # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature < 65:
+        return "It's a little chilly out there!"
+    elif 65 <= temperature <= 85:
+        return "It's perfect out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's not brisk out there."
+
     pass
 
 def fizzbuzz(num):
-    # your code here
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+
     pass
 
-def calculator(operation, num1, num2):
-    # your code here
-    pass
+def calculator(operator, num1, num2):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        if num2 == 0:
+            raise ValueError("Division by zero is not allowed")
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
+
+
+
